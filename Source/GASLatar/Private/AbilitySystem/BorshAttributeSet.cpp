1cd3ea95913d3d547fd33e18f0c9c8b2e308983f
Murad130 <66731914+Murad130@users.noreply.github.com>
2023-11-01 18:27:07 +0100

    Post Gameplay Effect Execute and Curve Tables

--- Source/GASLatar/Private/AbilitySystem/BorshAttributeSet.cpp
+++ Source/GASLatar/Private/AbilitySystem/BorshAttributeSet.cpp
@@ -2,6 +2,10 @@
 
 
 #include "AbilitySystem/BorshAttributeSet.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameFramework/Character.h"
+#include "GameplayEffectExtension.h"
 #include <Net/UnrealNetwork.h>
 
 UBorshAttributeSet::UBorshAttributeSet()
@@ -34,15 +38,76 @@ void UBorshAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute,
 	{
 		// We are making sure that our health does not get past MaxHealth(ex: when we pick up a potion, crystal etc)
 		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
-		
+
 	}
 	if (Attribute == GetManaAttribute())
 	{
 		// Same as for health but here it is for mana
 		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
-		
+
 	}
 
+}
+
+void UBorshAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
+{
+	// Source = causer of the effect, Target = target of the effect (owner of this AS)
+
+	// Getting our Effect Context
+	Props.EffectContextHandle = Data.EffectSpec.GetContext();
+	// Getting our Ability System Component from the source of this gameplay effect
+	Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
+	{
+		// Getting source actor that owns Ability System Component and storing it 
+		Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
+		// Getting our Player Controller
+		Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+		if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
+		{
+			// if SourceController is a null pointer SourceAvatarActor is not
+			// then we'll attempt to get the player controller from the pawn directly.
+
+			if (const APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
+			{
+				Props.SourceController = Pawn->GetController();
+				// So what we're doing here is we're first getting it from the ability actor info, but if ability actor info
+			// contains a null pointer for the source controller, well then we'll just try to get it from the actor
+			// by casting to a pawn and getting the controller then. So it's kind of a optional second step there.
+			}
+
+		}
+		// If Source Controller get the Source Character
+		if (Props.SourceController)
+		{
+			// Casting the pawn possessed by the controller to a character
+			Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
+		}
+
+		// By far we got a lot of info for Source Actor now we need to get info for the Target Actor
+
+		// Checking if these things are valid. If Valid we can get the target Controller
+		if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+		{
+			Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+			Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+			Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
+			Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
+
+		}
+	}
+
+
+}
+
+//Executed after a Gameplay Effect changes an Attribute
+void UBorshAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FEffectProperties Props;
+	SetEffectProperties(Data, Props);
 }
 
 void UBorshAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
@@ -64,3 +129,5 @@ void UBorshAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBorshAttributeSet, MaxMana, OldMaxMana);
 }
+
+
