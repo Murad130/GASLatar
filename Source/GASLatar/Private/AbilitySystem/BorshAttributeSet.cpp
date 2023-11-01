3e7d1e670e495d23799a6659c425d172ab26d807
Murad130 <66731914+Murad130@users.noreply.github.com>
2023-11-01 03:00:36 +0100

    Clamping Attributes

--- Source/GASLatar/Private/AbilitySystem/BorshAttributeSet.cpp
+++ Source/GASLatar/Private/AbilitySystem/BorshAttributeSet.cpp
@@ -18,6 +18,31 @@ void UBorshAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& O
 
 	DOREPLIFETIME_CONDITION_NOTIFY(UBorshAttributeSet, Health, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UBorshAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBorshAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBorshAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+// Clamping Attributes to prevent them going above MaxValue or belove MinValue
+// This function is called before Attribute Changes (ex: If our Health == MaxHealth 
+// and we pick up a potion, before adding Health this function will be called 
+// and in this example no heath will be added bcs Health == MaxHealth
+void UBorshAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		// We are making sure that our health does not get past MaxHealth(ex: when we pick up a potion, crystal etc)
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+		
+	}
+	if (Attribute == GetManaAttribute())
+	{
+		// Same as for health but here it is for mana
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		
+	}
+
 }
 
 void UBorshAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
--- Source/GASLatar/Public/AbilitySystem/BorshAttributeSet.h
+++ Source/GASLatar/Public/AbilitySystem/BorshAttributeSet.h
@@ -26,32 +26,34 @@ public:
 	UBorshAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
-		FGameplayAttributeData Health;
+	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(UBorshAttributeSet, Health);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
-		FGameplayAttributeData MaxHealth;
+	FGameplayAttributeData MaxHealth;
 	ATTRIBUTE_ACCESSORS(UBorshAttributeSet, MaxHealth);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
-		FGameplayAttributeData Mana;
+	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(UBorshAttributeSet, Mana);
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
-		FGameplayAttributeData MaxMana;
+	FGameplayAttributeData MaxMana;
 	ATTRIBUTE_ACCESSORS(UBorshAttributeSet, MaxMana);
 
 	UFUNCTION()
-		void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 
 	UFUNCTION()
-		void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
 
 	UFUNCTION()
-		void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
 
 	UFUNCTION()
-		void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 
 };
