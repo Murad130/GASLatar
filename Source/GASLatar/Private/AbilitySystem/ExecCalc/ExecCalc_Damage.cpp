bb43a289c88c35197979ea43431443de8f360459
Murad130 <66731914+Murad130@users.noreply.github.com>
2023-11-15 23:49:45 +0100

    Implementing Block Chance

--- Source/GASLatar/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/GASLatar/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "BorshGameplayTags.h"
 #include "AbilitySystem/BorshAttributeSet.h"
 
 struct BorshDamageStatics
@@ -11,11 +12,13 @@ struct BorshDamageStatics
 	// This is a kind of shorthand version of doing what we did in the MMC_MaxHealth class for Vigor
 	// Well, we want to declare some new capture definitions and there's a macro for that.
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 
 	BorshDamageStatics()
 	{
 		// Anything we put here in BorshDamageStatics, if we call DamageStatics to access it, then we'll get the same one every time.
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBorshAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBorshAttributeSet, BlockChance, Target, false);
 		// But we also have to add this to a list, specifically an array on the calc damage class to tell this execution calculation class that this capture def should be used to capture that specific attribute.
 		// We need to do this in our Constructor (in UExecCalc_Damage::UExecCalc_Damage())
 	}
@@ -31,6 +34,7 @@ static const BorshDamageStatics& DamageStatics()
 UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -49,15 +53,21 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
 
-	// Now the question how do we actually capture it here in our execution calculation function execute? How are we going to actually get that armor definition? Well, the way to do it is through a function on execution params
-	float Armor = 0.f;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, Armor);
-	Armor = FMath::Max<float>(0.f, Armor); // So this would make sure that armor is never a negative value
-	++Armor;
+	// Get Damage Set By Caller Magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FBorshGameplayTags::Get().Damage);
+
+	// Capture Block Chance on Target, and determine if there was a successfull Block
+	// If Block, halve the damage.
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
+	
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bBlocked ? Damage / 2.f : Damage; // same as if (bBlocked) Damage /= 2.f;
 
 	// So one of the last things to know how to do is how do we determine what exactly happens as a result of this execution calculation ? execution output is related to that.
 	// We modify attributes using OutExecutionOutput parameter of this function.
-	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor); // Now all we're doing here is taking the value of armor and passing it into this evaluated data. We're not actually changing its value
+	const FGameplayModifierEvaluatedData EvaluatedData(UBorshAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage); 
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 
 }
