94c9ce1682e7f38ea1ccd54e3aa4308dc09d367b
Murad130 <66731914+Murad130@users.noreply.github.com>
2023-11-16 00:06:09 +0100

    Implementing Armor and Armor Penetration

--- Source/GASLatar/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/GASLatar/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -12,6 +12,7 @@ struct BorshDamageStatics
 	// This is a kind of shorthand version of doing what we did in the MMC_MaxHealth class for Vigor
 	// Well, we want to declare some new capture definitions and there's a macro for that.
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 
 	BorshDamageStatics()
@@ -19,6 +20,7 @@ struct BorshDamageStatics
 		// Anything we put here in BorshDamageStatics, if we call DamageStatics to access it, then we'll get the same one every time.
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBorshAttributeSet, Armor, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UBorshAttributeSet, BlockChance, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBorshAttributeSet, ArmorPenetration, Source, false);
 		// But we also have to add this to a list, specifically an array on the calc damage class to tell this execution calculation class that this capture def should be used to capture that specific attribute.
 		// We need to do this in our Constructor (in UExecCalc_Damage::UExecCalc_Damage())
 	}
@@ -35,6 +37,7 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -57,13 +60,28 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	float Damage = Spec.GetSetByCallerMagnitude(FBorshGameplayTags::Get().Damage);
 
 	// Capture Block Chance on Target, and determine if there was a successfull Block
-	// If Block, halve the damage.
 	float TargetBlockChance = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
 	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
 	
 	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	// If Block, halve the damage.
 	Damage = bBlocked ? Damage / 2.f : Damage; // same as if (bBlocked) Damage /= 2.f;
+
+	// Capture Armor 
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor, 0.f);
+
+	// Capture ArmorPenetration.
+	float SourceArmorPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef, EvaluationParameters, SourceArmorPenetration);
+	SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration, 0.f);
+
+	// ArmorPenetration ignores a percentage of the target Armor.
+	const float EffectiveArmor = TargetArmor *= (100 - SourceArmorPenetration * 0.25f) / 100.f;
+	// Armor ignore a percentage of incoming damage
+	Damage *= (100 - EffectiveArmor * 0.333f) / 100.f;
 
 	// So one of the last things to know how to do is how do we determine what exactly happens as a result of this execution calculation ? execution output is related to that.
 	// We modify attributes using OutExecutionOutput parameter of this function.
